49a0859e5352a78648b9c5186c31dfa90f734b97
matarillo <kentaro.inomata@gmail.com>
2025-12-22 16:17:05 +0900

    Initializr

---
+++ src/main/java/com/example/flaskr/FlaskrApplication.java
@@ -0,0 +1,13 @@
+package com.example.flaskr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlaskrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlaskrApplication.class, args);
+	}
+
+}
