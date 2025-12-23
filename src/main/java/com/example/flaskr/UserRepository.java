b7c571871b5fd449c3a4efeff51aba9412567e14
matarillo <kentaro.inomata@gmail.com>
2025-12-23 10:13:14 +0900

    remove unused import

--- src/main/java/com/example/flaskr/UserRepository.java
+++ src/main/java/com/example/flaskr/UserRepository.java
@@ -1,6 +1,5 @@
 package com.example.flaskr;
 
-import org.springframework.data.relational.core.mapping.Table;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,5 +8,5 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends CrudRepository<User, Integer> {
 
-    public Optional<User> findByUsername(String username);
+    Optional<User> findByUsername(String username);
 }
