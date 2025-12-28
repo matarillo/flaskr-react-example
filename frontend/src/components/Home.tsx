a774133fd620390ab75fadeaff508da8a57606ce
matarillo <kentaro.inomata@gmail.com>
2025-12-28 11:09:02 +0900

    create

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,15 +1,19 @@
+import { useAuth } from "../contexts/auth";
 import PostList from "./PostList";
 
 function Home() {
+  const { user } = useAuth();
+
   return (
     <>
       <title>Posts - Flaskr</title>
       <header>
         <h1>Posts</h1>
+        {user && <a className="action" href="/create">New</a>}
       </header>
       <PostList />
     </>
-  )
+  );
 }
 
 export default Home;
